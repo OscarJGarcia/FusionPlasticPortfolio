f46800e5ec275783eee01b3bf11a968144ceed63
OscarJGarcia <razor1223@hotmail.com>
2023-01-19 11:37:52 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from "./container/Header/Header";
+
+function App() {
+  return (
+    <div className="Main">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/container/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function Header() {
+    return (
+        <div className="header">
+        </div>
+    );
+}
+
+export default Header;
