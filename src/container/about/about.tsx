f2a7f7ff8b24ef5bced92a836ead0e0be90330ec
OscarJGarcia <OscarJavier.GarciaGarcia@marcusmillichap.com>
2023-02-14 12:09:32 -0500

    Update: change about page

--- src/container/about/about.tsx
+++ src/container/about/about.tsx
@@ -11,7 +11,7 @@ function About() {
             </div>
             <div className="description">
                 <p>
-                    Desde el año 2021 Somos una empresa colombiana que ofrece la creación de productos para los sectores farmacéutico, cosmético e industrial; fabricando productos plásticos con material certificado de alta calidad que puedan satisfacer las necesidades del mercado nacional. Como empresa nueva nos caracterizamos por la excelente calidad de nuestros productos a menor precio. En Fusion Plastic contamos con personal calificado, cumpliendo con todas las normas de la industria para poder brindar nuestras soluciones.
+                    Desde el año 2021 Somos una empresa colombiana que ofrece la creación y distribución de productos para los sectores farmacéutico, cosmético e industrial; fabricando productos plásticos con material certificado de alta calidad que puedan satisfacer las necesidades del mercado nacional. Como empresa nueva nos caracterizamos por la excelente calidad de nuestros productos a menor precio. En Fusion Plastic contamos con personal calificado, cumpliendo con todas las normas de la industria para poder brindar nuestras soluciones.
                 </p>
             </div>
         </div>
